e41f7021c13bd03b9f4e752690cca4be2c728f8d
Shawn Paulsen <shawnpaulsen@Shawns-MacBook-Pro.local>
2021-04-23 17:50:57 -0600

    index.js working

--- index.js
+++ index.js
@@ -1,194 +1,148 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateHTML = require('./src/generateHTML');
-const Manager = require('./lib/manager');
-const Engineer = require('./lib/engineer');
-const Intern = require('./lib/intern');
+const fs = require("fs");
+
+const generateHTML = require("./src/generateHTML");
+
+const inquirer = require("inquirer");
+
+const Manager = require("./lib/Manager");
+
+const Engineer = require("./lib/Engineer");
+
+const Intern = require("./lib/Intern");
+
 
 const teamMembers = [];
 
-const askManager = [{
-        type: 'imput',
-        name: 'name',
+const questionsManager = [{
+        type: "input",
+        name: "name",
         message: "What is the team manager's name?",
     },
     {
-        type: 'imput',
-        name: 'id',
-        message: 'What is your manager id?',
+        type: "input",
+        name: "id",
+        message: "What is your manager id?",
     },
     {
-        type: 'imput',
-        name: 'email',
-        message: 'What is your email?',
+        type: "input",
+        name: "email",
+        message: "What is your email?",
     },
     {
-        type: 'imput',
-        name: 'officeNumber',
-        message: 'What is your office number?',
+        type: "input",
+        name: "officeNumber",
+        message: "What is your office number?",
     },
     {
-        type: 'imput',
-        name: 'members',
-        message: 'Select the role of any team member to be added to the directory.',
-        choices: ['Engineer', 'Employee', 'Intern', 'None to add'],
+        type: "list",
+        name: "members",
+        message: "Select the role of any team member to be added to the directory.",
+        choices: ["Engineer", "Intern", "I'm done."],
     },
 ];
 
 const askEngineer = () => {
     inquirer
         .prompt([{
-            type: 'imput',
-            name: 'name',
-            message: "What is the engineer's name?",
-        },
-        {
-            type: 'imput',
-            name: 'id',
-            message: 'What is your engineer id?',
-        },
-        {
-            type: 'imput',
-            name: 'email',
-            message: 'What is your email?',
-        },
-        {
-            type: 'imput',
-            name: 'github',
-            message: 'What is your GitHub user name?',
-        },
-        {
-            type: 'imput',
-            name: 'members',
-            message: 'Select the role of any team member to be added to the directory.',
-            choices: ['Engineer', 'Employee', 'Intern', 'None to add'],
-        },
-        
-    ])
-    then((data) => {
-        teamMembers.push(
-            new Engineer(data.name, data.id, data.email, data.github)
-        );
+                type: "input",
+                name: "name",
+                message: "What is the engineer's name?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your engineer id?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your email?",
+            },
+            {
+                type: "input",
+                name: "GitHub",
+                message: "What is your GitHub username?",
+            },
+            {
+                type: "list",
+                name: "members",
+                message: "Which type of team member would you like to add?",
+                choices: ["Engineer", "Intern", "None to add."],
+            },
+        ])
+        .then((data) => {
+            teamMembers.push(
+                new Engineer(data.name, data.id, data.email, data.GitHub)
+            );
+            if (data.members === "Engineer") {
+                askEngineer();
+            } else if (data.members === "Intern") {
+                askIntern();
+            } else {
+                let data = generateHTML(teamMembers);
+                fs.writeFileSync("team.html", data, "utf-8");
+            }
+        });
+};
 
+const askIntern = () => {
+    inquirer
+        .prompt([{
+                type: "input",
+                name: "name",
+                message: "What is your name?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your intern id?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your email?",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is your school?",
+            },
+            {
+                type: "list",
+                name: "members",
+                message: "Which type of team member would you like to add?",
+                choices: ["Engineer", "Intern", "None to add."],
+            },
+        ]).then((data) => {
+            teamMembers.push(
+                new Intern(data.name, data.id, data.email, data.school)
+            );
+            if (data.members === "Engineer") {
+                askEngineer();
+            } else if (data.members === "Intern") {
+                askIntern();
+            } else {
+                let data = generateHTML(teamMembers);
+                fs.writeFileSync("team.html", data, "utf-8");
+            }
+        });
+};
+
+const init = () => {
+    inquirer.prompt(questionsManager).then((data) => {
+        teamMembers.push(
+            new Manager(data.name, data.id, data.email, data.officeNumber)
+        );
         if (data.members === "Engineer") {
             askEngineer();
         } else if (data.members === "Intern") {
             askIntern();
         } else {
             let data = generateHTML(teamMembers);
-            fs.writeFileSync('team.html', data, 'utf-8');
-
+            fs.writeFileSync("team.html", data, "utf-8");
         }
     });
 };
-    
-const askIntern = () => {
-    inquirer
-        .prompt([{
-            type: 'imput',
-            name: 'name',
-            message: 'What is the team managers name?',
-        },
-        {
-            type: 'imput',
-            name: 'name',
-            message: 'What is the team managers name?',
-        },
-        {
-            type: 'imput',
-            name: 'name',
-            message: 'What is the team managers name?',
-        },
-        {
-            type: 'imput',
-            name: 'name',
-            message: 'What is the team managers name?',
-        },
-        {
-            type: 'imput',
-            name: 'name',
-            message: 'What is the team managers name?',
-        },
-        {
-            type: 'imput',
-            name: 'name',
-            message: 'What is the team managers name?',
-        },
-        {
-            type: 'imput',
-            name: 'name',
-            message: 'What is the team managers name?',
-        },
-        {
-            type: 'imput',
-            name: 'name',
-            message: 'What is the team managers name?',
-        },
-    ])
-}   
-then((data) => {
-    teamMembers.push(
-        new Engineer(data.name, data.id, data.email, data.github)
-    );
-
-    if (data.members === "Engineer") {
-        askEngineer();
-    } else if (data.members === "Intern") {
-        askIntern();
-    } else {
-        let data = generateHTML(teamMembers);
-        fs.writeFileSync('team.html', data, 'utf-8');
-
-    }
-});
 
 
-        
-
-        // const generateHTML = (answers) =>
-        //     `<!DOCTYPE html>
-        //   <html lang="en">
-        //   <head>
-        //     <meta charset="UTF-8">
-        //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        //     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        //     <title>Document</title>
-        //   </head>
-        //   <body>
-        //     <div class="jumbotron jumbotron-fluid">
-        //     <div class="container">
-        //       <h1 class="display-4">Hi! My name is ${answers.title}</h1>
-        //       <p class="lead">I am from ${answers.description}.</p>
-        //       <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-        //       <ul class="list-group">
-        //         <li class="list-group-item">My GitHub username is ${answers.question}</li>
-        //         <li class="list-group-item">LinkedIn: ${answers.question2}</li>
-        //       </ul>
-        //     </div>
-        //   </div>
-        //   </body>
-        //   </html>`;
-
-        // const init = () => {
-        //     promptUser()
-        //         .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
-        //         .then(() => console.log('Successfully wrote to index.html'))
-        //         .catch((err) => console.error(err));
-        // };
-
-        // init();
-
-        // .then(function (response) {
-        //     fs.writeFile(path.join(process.cwd(), 'team.html'),
-
-        // ``
-        // , (err) =>
-        //         err ? console.log(err) : console.log('README created successfully!'))
-
-        // })
-
-        // function init() {
-
-        // }
-
-        // init();
+init();
